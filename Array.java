c587a86a6db7954cc7e6a9cd6ef18fc1d9b7a946
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-01-02 07:50:28 +0000

    Practicing

---
+++ Array.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Array{
+    public static void main(String[] args){
+        int[] array = new int[5];
+        System.out.println(array);
+    }
+}
---
+++ SubsetSum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SubsetSum{
+    public static void main(String[] args){
+
+    }
+
+    public static boolean calcSum(int[] array, int length, int target){
+        int sum = 0;
+        if(sum == target) return true;
+
+        
+    }
+}
